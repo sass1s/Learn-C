53522f1bab094d992f5af040e78632b07441ef18
sass1s <2063198253@qq.com>
2015-11-19 21:12:04 +0800

    modify files in Chapter15

--- Chapter15/test1.c
+++ Chapter15/test1.c
@@ -16,6 +16,13 @@ void main(void)
 {
 	printf("%lld\n",1234567890ll*1234567890);
 	int i;
-	for (i=5;i<25;i++)	
-		printf("%lld\n",my_fac(i));
+	/*for (i=5;i<25;i++)	
+		printf("%lld\n",my_fac(i));*/
+	unsigned char c1=255,c2=2;
+	char n=c1+c2;
+	printf("%d\n",n);
+	int c=3.14;
+	printf("%f\n",(double)3+c);
+	unsigned short s=65536;
+	printf("%d\n",s);
 }
